f1b6bae76b2b9cbcafc05070dc944d4c1ab82158
Prachanda Rana <prana@supremeitsolutions.com>
2024-12-28 00:35:43 +0545

    Array using class

--- array.js
+++ array.js
@@ -1,27 +1,56 @@
-const storedUser = ["ramesh", "suresh", "raju", "akhil"]
-const storedUser2 = ["ss", "sdfsdf", "raju", "akhila"]
+class MyArray {
+    constructor() {
+        this.data = {}
+        this.length = 0
+    }
+    push(item) {
+        this.data[this.length] = item
+        this.length++
+            return this.length
+    }
+    pop() {
+        const lastItem = this.data[this.length - 1]
+        delete this.data[this.length - 1]
+        this.length--
+            return lastItem
 
-// helper function
-const findUser = (array, user) => {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === user) {
-            return `${user} found on index ${i}`
+    }
+    unshift(item) {
+        for (let i = this.length; i > 0; i--) {
+            this.data[i] = this.data[i - 1]
         }
+        this.data[0] = item
+        this.length++
+    }
+    shift() {
+        for (let i = 0; i < this.length; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+        // remove the last index as it is undefined
+        delete this.data[this.length - 1]
+        this.length--
+    }
+    get(index) {
+        console.log(this.data[index])
+    }
+    deleteByIndex(index) {
+        console.log(this.length)
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+        delete this.data[this.length - 1]
+        this.length--
     }
 }
 
-
-
-// console.log(findUser(storedUser, "akhil"))
-
-
-const rows = 5;
-const cols = 5;
-
-for (let i = 1; i <= rows; i++) {
-    let row = "";
-    for (let j = 1; j <= cols; j++) {
-        row += (i * j) + "\t"; // Multiplication table values
-    }
-    console.log(row);
-}
+const myArr = new MyArray()
+myArr.push(50)
+myArr.push(5)
+myArr.push("hello")
+    // myArr.unshift("world")
+    // myArr.shift()
+    // myArr.pop()
+console.log(myArr)
+console.log(myArr.deleteByIndex(1))
+myArr.unshift("new")
+    // console.log(myArr)
