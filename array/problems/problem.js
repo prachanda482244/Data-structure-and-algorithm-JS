7cafc59f8387e13c9a4f84988eba9ddfe425f716
Prachanda Rana <prana@supremeitsolutions.com>
2024-12-28 21:30:30 +0545

    check palindrome

--- array/problems/problem.js
+++ array/problems/problem.js
@@ -7,8 +7,26 @@ const reverseString = (string) => {
     console.log(reversedAr.join(""))
 }
 
-reverseString("hello")
+// reverseString("hello")
 
 const reverseStringUsingBuiltIn = str => str.split("").reverse().join("")
 
-console.log(reverseStringUsingBuiltIn("hello"))
+// console.log(reverseStringUsingBuiltIn("hello"))
+
+const isPalindrome = str => {
+    let storeString = str.split("")
+    let array = []
+    for (let i = storeString.length; i >= 0; i--) {
+        array.push(storeString[i])
+    }
+    if (str === array.join("")) {
+        console.log("Its palindrome")
+    } else {
+        console.log("not a palindrome")
+    }
+}
+
+const anotherWayPalindrome = str => str === str.split("").reverse().join("") ? "Its palindrome" : "Not palindrome"
+
+console.log(isPalindrome("racecar"))
+console.log(anotherWayPalindrome("racecarw"))
