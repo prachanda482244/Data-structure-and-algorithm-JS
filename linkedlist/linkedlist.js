ca51aa3c12a3bd1125f11410f13dbe880ebdfed4
Prachanda Rana <prana@supremeitsolutions.com>
2025-01-16 21:06:29 +0545

    get index in linkedlist

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -1,92 +1,126 @@
 class Node {
     constructor(value, next = null) {
-        this.head = value;
-        this.next = next
+        this.value = value;
+        this.next = next;
     }
-
 }
+
 class LinkedList {
     constructor(value) {
-        this.head = new Node(value)
-        this.tail = this.head
-        this.length = 1
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
     }
+
     push(value) {
-        let newNode = new Node(value)
+        const newNode = new Node(value);
         if (!this.head) {
+            this.head = this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
             this.tail = newNode;
-            this.head = newNode
         }
-        this.tail.next = newNode;
-        this.tail = newNode
-        this.length++
+        this.length++;
+        return this;
+        // Allow method chaining
     }
+
     pop() {
+        if (!this.head) return null; // Empty list check
+
+        if (this.length === 1) { // Single node case
+            const poppedNode = this.head;
+            this.head = this.tail = null;
+            this.length--;
+            return poppedNode;
+        }
+
+        let current = this.head;
+        let previous = null;
+
+        while (current.next) {
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = null;
+        this.tail = previous;
+        this.length--;
+
+        return current; // Return the removed node
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
         if (!this.head) {
-            return
+            this.head = this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
         }
-        this.temp = this.head
-        this.prev = this.head
-        while (this.temp.next) {
-            this.prev = this.temp
-            this.temp = this.prev.next
-        }
-        this.tail = this.prev;
-        this.tail.next = null
-        this.length--
+        this.length++;
+        return this;
+    }
+
+    shift() {
+        if (!this.head) return null;
+
+        const removedNode = this.head;
+        this.head = this.head.next;
+        this.length--;
 
         if (this.length === 0) {
-            this.head = null;
-            this.tail = null
+            this.tail = null;
         }
-        return this.temp
+
+        return removedNode;
     }
-    unshift(value) {
-        let newNode = new Node(value)
-        if (!this.head) {
-            this.head = newNode
-            this.tail = newNode
-        }
-        newNode.next = this.head
-        this.head = newNode
-        this.length++
-        return this
-    }
-    shift() {
-        if (!this.head) {
-            return null
-        }
-        let temp = this.head
-        this.head = this.head.next
-        this.length--
-        if (this.length === 0) {
-            this.tail = null
-        }
-        return temp
-    }
+
     getFirst() {
-        return this.head
+        return this.head || null;
     }
+
     getLast() {
-        if (!this.head) return null
-        let temp = this.head
-        while (temp.next) {
-            if (!temp.next) return temp
-            temp = temp.next
+        return this.tail || null;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+
+        let current = this.head;
+        let counter = 0;
+
+        while (counter < index) {
+            current = current.next;
+            counter++;
         }
+
+        return current;
+    }
+
+
+    printList() {
+        const values = [];
+        let current = this.head;
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+        return values;
     }
 }
 
-const linkedlist = new LinkedList(2)
-
-linkedlist.push(3)
-linkedlist.push(30)
-// linkedlist.push(300)
-// linkedlist.pop()
-// linkedlist.pop()
-// linkedlist.pop()
-// linkedlist.unshift(20)
-// linkedlist.shift()
-// linkedlist.getFirst()
-linkedlist.getLast()
-console.log(linkedlist, "last")
+// Example usage
+const linkedList = new LinkedList(2);
+linkedList.push(3).push(30); // Method chaining
+console.log("List after push:", linkedList.printList());
+console.log("Last Node:", linkedList.getLast().value);
+linkedList.pop();
+console.log("List after pop:", linkedList.printList());
+linkedList.unshift(1);
+console.log("List after unshift:", linkedList.printList());
+linkedList.shift();
+console.log("List after shift:", linkedList.printList());
+linkedList.get(1)
+console.log("Get specific index :", linkedList.get(1));
